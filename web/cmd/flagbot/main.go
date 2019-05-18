d45c19066ac50841392da2dbb1580d9e5299767b
Joe Rozner <joe@deadbytes.net>
2019-05-18 15:11:04 -0700

    Add flag solving

--- web/cmd/flagbot/main.go
+++ web/cmd/flagbot/main.go
@@ -7,7 +7,11 @@ import (
 	"strings"
 	"time"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+
 	"github.com/jrozner/the-intercept-2019/web/client"
+	"github.com/jrozner/the-intercept-2019/web/model"
 )
 
 var flags map[string]Flag
@@ -16,6 +20,15 @@ func main() {
 	config, err := readConfig()
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	db, err := gorm.Open("mysql", config.DSN)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if config.Debug {
+		db.LogMode(true)
 	}
 
 	flags, err = readFlags()
@@ -45,7 +58,7 @@ func main() {
 				command = message.Text
 			} else {
 				command = message.Text[0:commandIndex]
-				remainder = message.Text[commandIndex:]
+				remainder = message.Text[commandIndex+1:]
 			}
 
 			switch command {
@@ -56,7 +69,10 @@ func main() {
 			case "challenge":
 				sendChallenge(c, recipient, remainder)
 			case "solve":
+				solveChallenge(db, c, recipient, remainder)
 			case "scoreboard":
+			default:
+				log.Println("invalid command: ", command)
 			}
 
 			err = c.DeleteMessage(message.ID)
@@ -100,6 +116,69 @@ func sendChallenge(c *client.Client, recipient string, challenge string) {
 	}
 
 	err := c.SendMessage(recipient, fmt.Sprintf("%s - %d\n\n%s", challenge, flag.Points, flag.Hint))
+	if err != nil {
+		log.Println("failed to respond to user: ", err)
+	}
+}
+
+func solveChallenge(db *gorm.DB, c *client.Client, recipient string, message string) {
+	parts := strings.Split(message, ":")
+	if len(parts) != 2 {
+		log.Println("malformed solution")
+		err := c.SendMessage(recipient, "malformed solution")
+		if err != nil {
+			log.Println("failed to respond to user: ", err)
+		}
+
+		return
+	}
+
+	flag, ok := flags[parts[0]]
+	if !ok {
+		log.Printf("Challenge '%s' doesn't exist\n", parts[0])
+		err := c.SendMessage(recipient, fmt.Sprintf("invalid challenge: %s", parts[0]))
+		if err != nil {
+			log.Println("failed to respond to user: ", err)
+		}
+
+		return
+	}
+
+	// hey, there's a timing attack possible here. Good fucking luck
+	if parts[1] != flag.Value {
+		log.Println("invalid flag")
+		err := c.SendMessage(recipient, fmt.Sprintf("invalid flag"))
+		if err != nil {
+			log.Println("failed to respond to user: ", err)
+		}
+
+		return
+	}
+
+	var user model.User
+	err := db.Model(&user).First(&user, "team_name = ?", recipient).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			log.Printf("sender %s doesn't exist\n", recipient)
+			return
+		}
+
+		log.Println(err)
+		return
+	}
+
+	newFlag := &model.Flag{
+		Challenge: parts[0],
+		Points:    flag.Points,
+		UserID:    user.ID,
+	}
+
+	err = db.Create(&newFlag).Error
+	if err != nil {
+		log.Println(err)
+	}
+
+	err := c.SendMessage(recipient, "flag accepted")
 	if err != nil {
 		log.Println("failed to respond to user: ", err)
 	}
