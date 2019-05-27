599bac70836c537f16177db8f2feb2621cd8e3d3
Joe Rozner <joe@deadbytes.net>
2019-05-26 19:10:48 -0700

    Add the flags

--- web/flags.sql
+++ web/flags.sql
@@ -1,7 +1,13 @@
 insert into flags (name, hint, value, points, created_at, updated_at) values
-  ('Dive Right In', 'Find the device''s serial number. Flag format: flag{SERIAL}', 'CT-923456A', 25, now(), now()),
-  ('Fish Fry', 'Find the device''s processor/module serial number. Flag format: flag{SERIAL-SERIAL-SERIAL}', 'ESP32--WROOM-320', 35, now(), now()),
-  ('Catch of the Day', 'Find the device''s firmware version string. Flag format: flag{version_string}', 'COOLTUNA v1.34.5.1.2', 50, now(), now()),
-  ('Cast Your Nets', 'Find the device''s wireless access point SSID. Flag format: flag{SSID}', 'interceptctfnet', 75, now(), now()),
-  ('MFTUNA', 'Bypass COOLTUNAs on-boot security mechanisms.', 'PLACEHOLDER', 50, now(), now()),
-  ('MACkerel Fishing', 'Find your device''s MAC address and extract the OUI. Flag format: flag{0123AB} (uppercase hex!)', '246F28', 50, now(), now());
+  ('Fish Fry', 'Find the device''s processor/module serial number', 'ESP32-WROOM-32D', 35, now(), now()),
+  ('Catch of the Day', 'Find the device''s firmware version string', 'COOLTUNA v1.34.5.1.2', 50, now(), now()),
+  ('Cast Your Nets', 'Find the device''s wireless access point SSID', 'interceptctfnet2', 75, now(), now()),
+  ('MFTUNA', 'Bypass COOLTUNAs on-boot security mechanisms.', 'MFTUNAup1Nthis', 50, now(), now()),
+  ('MACkerel Fishing', 'Find your device''s MAC address and extract the OUI(uppercase hex!)', '246F28', 50, now(), now()),
+  ('Under the Sea', 'Dump the device''s firmware and this slippery fish should be easy to spot.', 'trawlingTHEDepthsISea', 100, now(), now()),
+  ('Hacker Tunaversity', 'Bypass COOLTUNAs authentication to unlock access to deeper waters', 'KINGoftHeMACKERELs', 150, now(), now()),
+  ('The Cartunaist', 'Every artist signs their work. Find COOLTUNA''s signature.', 'Br34kTHEsc4l3', 125, now(), now()),
+  ('You''re Krilling Me!', 'This barnacle encrusted hardware is hiding a serial number. Can you find it?', '93LC56BI', 50, now(), now()),
+  ('The Clownfish', 'Find Cool Tuna''s best joke!', 'findThemFISHJokeZ', 150, now(), now()),
+  ('Gotta Catch Em All', 'COOLTUNA devs forgot to enable some functionality, but the code is still there. Find it to find a flag!', 'hiddenAmongTHEReeds', 200, now(), now()),
+  ('Don''t Be So Shellfish', 'We may have seen the firmware before. What''s the sha2sum of the main application?', '4fbbb8fa89671dab859b1cdf36c1cf6c954df54e39fdaa464bdde03a95e756a7', 50, now(), now());
